4c3d8fb4e61487b1c0b24962c13b39191fc6ec2e
PublicSVM <20406905+PublicSVM@users.noreply.github.com>
2020-10-18 22:17:01 +0200

    cleanup

--- Rougelite Project/Assets/Game/Scripts/GridGeneration.cs
+++ Rougelite Project/Assets/Game/Scripts/GridGeneration.cs
@@ -1,20 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class GridGeneration : MonoBehaviour
 {
+    [Header("World size")]
+    [SerializeField] private int _width = 16;
+    [SerializeField] private int _depth = 9;
+
+    [Header("Prefabs")]
     [SerializeField] private GameObject _tilePrefab;
+
+    private List<GameObject> _tiles;
 
     private void Start()
     {
-        for (var z = 0; z < 10; z++)
+        GenerateWorld();
+    }
+
+    private void GenerateWorld()
+    {
+        DestroyWorld();
+
+        for (var z = 0; z < _depth; z++)
         {
-            for (var x = 0; x < 5; x++)
+            for (var x = 0; x < _width; x++)
             {
-                var tile = Instantiate(_tilePrefab, new Vector3(x, 0, z), Quaternion.identity, this.transform);
+                var position = new Vector3(x - _width / 2f, 0, z - _depth / 2f);
+                var tile = Instantiate(_tilePrefab, position, Quaternion.identity, this.transform);
                 tile.name = $"{x},{z}";
+
+                _tiles.Add(tile);
             }
+        }
+    }
+
+    private void DestroyWorld()
+    {
+        foreach (var tile in _tiles)
+        {
+            Destroy(tile);
         }
     }
 }

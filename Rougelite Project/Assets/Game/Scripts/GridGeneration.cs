853b0703fdc3758a22f548aedf02a7633222a1ae
PublicSVM <20406905+PublicSVM@users.noreply.github.com>
2020-10-18 21:45:02 +0200

    add a basic tiled environment

---
+++ Rougelite Project/Assets/Game/Scripts/GridGeneration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGeneration : MonoBehaviour
+{
+    [SerializeField] private GameObject _tilePrefab;
+
+    private void Start()
+    {
+        for (var z = 0; z < 10; z++)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                var tile = Instantiate(_tilePrefab, new Vector3(x, 0, z), Quaternion.identity, this.transform);
+                tile.name = $"{x},{z}";
+            }
+        }
+    }
+}

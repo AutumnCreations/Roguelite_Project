795e38e654d97d8be8e37d16575fa4c53444d468
PublicSVM <20406905+PublicSVM@users.noreply.github.com>
2020-10-18 23:06:01 +0200

    add player movement

---
+++ Rougelite Project/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int X = 0;
+    public int Z = 0;
+
+    public World World;
+
+    private void Start()
+    {
+        var tile = World.GetTileAt(X, Z);
+        this.transform.position = tile.transform.position + new Vector3(0, 0.55f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Move(0, 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(-1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(0, -1);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(1, 0);
+        }
+    }
+
+    public void Move(int horizontal, int vertical)
+    {
+        var tileObject = World.GetTileAt(X + horizontal, Z + vertical);
+        if (tileObject is null)
+        {
+            return;
+        }
+
+        var targetPosition = tileObject.transform.position + new Vector3(0, 0.55f);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, 1);
+
+        var tile = tileObject.GetComponent<WorldTile>();
+        this.X = tile.X;
+        this.Z = tile.Z;
+    }
+}
---
+++ Rougelite Project/Assets/Game/Scripts/World.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    [Header("World size")]
+    [SerializeField] private int _width = 16;
+    [SerializeField] private int _depth = 9;
+
+    [Header("Prefabs")]
+    [SerializeField] private GameObject _tilePrefab;
+
+    private readonly List<GameObject> _tiles = new List<GameObject>();
+
+    private void Awake()
+    {
+        GenerateWorld();
+    }
+
+    private void GenerateWorld()
+    {
+        DestroyWorld();
+
+        for (var z = 0; z < _depth; z++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                var position = new Vector3(x - _width / 2f - 0.5f, 0, z - _depth / 2f - 0.5f);
+                var tileObject = Instantiate(_tilePrefab, position, Quaternion.identity, this.transform);
+                tileObject.name = $"{x},{z}";
+                _tiles.Add(tileObject);
+
+                var tile = tileObject.AddComponent<WorldTile>();
+                tile.X = x;
+                tile.Z = z;
+            }
+        }
+    }
+
+    private void DestroyWorld()
+    {
+        foreach (var tile in _tiles)
+        {
+            Destroy(tile);
+        }
+    }
+
+    public GameObject GetTileAt(int x, int z)
+    {
+        return _tiles.SingleOrDefault(t =>
+        {
+            var tile = t.GetComponent<WorldTile>();
+            return tile.X == x && tile.Z == z;
+        });
+    }
+}
+
+public class WorldTile : MonoBehaviour
+{
+    public int X;
+    public int Z;
+}
